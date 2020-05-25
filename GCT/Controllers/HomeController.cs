a73bc87305ed37cb2bc619991704571e31944ec8
Anil Pandey <anil.kumar.pandey@marsh.com>
2020-05-25 15:38:21 +0530

    Added new Login and Dashboard pages

--- GCT/Controllers/HomeController.cs
+++ GCT/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using GCT.Models;
+using Newtonsoft.Json;
+
 
 namespace GCT.Controllers
 {
@@ -25,6 +29,62 @@ namespace GCT.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            ViewBag.Message = "";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:61633/api/login/login");                
+                var responseTask =  client.PostAsJsonAsync("Login", model);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var userString = result.Content.ReadAsStringAsync().Result;
+
+                    var userResult = new
+                    {
+                        ID = "",
+                        Name = ""
+                    };
+                    var jsonResult = JsonConvert.DeserializeObject(userString);
+                    var user = JsonConvert.DeserializeAnonymousType(jsonResult.ToString(),userResult);
+                    if (user.ID != "0")
+                    {
+                        return View("Dashboard");
+                       
+                    }
+                    else //web api sent error response 
+                    {
+                        ViewBag.Message = "Incorrect User Name or Password";
+                        return View("login");
+                    }
+                }
+                else //web api sent error response 
+                {
+                    ViewBag.Message = "Unable to contact server! Please try after some time.";
+                    return View("login");
+                }
+                return View();
+            }
+           
+        }
+
+        public ActionResult Cancel()
+        {
+            return View("Index");
         }
     }
 }
